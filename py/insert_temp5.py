83bd5b5ff3530390f5b6fdcc3a1ccb4926c6a7d4
eploentham <eploentham@gmail.com>
2015-10-19 18:03:10 +0700

    581019-1

--- py/insert_temp5.py
+++ py/insert_temp5.py
@@ -2,6 +2,7 @@
 
 import os
 import cgi, cgitb
+import ConfigParser
 from datetime import datetime
 
 cgitb.enable()
@@ -9,16 +10,13 @@ cgitb.enable()
 form = cgi.FieldStorage()
 # Get data from fields
 devi1 = form.getvalue('devi1')
-#devt = form.getvalue('devt1')
+nodemcuid = form.getvalue('nodemcuid')
 val1 = form.getvalue('val1')
 #devi = escape(devi)
 #val1 = escape(val1)
 #date=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 #date=str(date)
 #txt=devi+","+devt+","
-#rem="1"
-#unit1="1"
-#value1="1"
 try:
         f = open('/usr/lib/cgi-bin/temp5.io', 'w')
 #f.write(str(devi1)+","+str(date)+","+str(val1))
@@ -26,25 +24,76 @@ try:
         f.close()
 #t_device (id varchar(255), device_id varchar(255), device_type varchar(255), d$
 #       curs.execute("INSERT into t_device values("+id+","+ devi+","+devt+", no$
+        config = ConfigParser.ConfigParser()
+        config.read('/home/ekapop/build-timerwater-Desktop_Qt_5_5_0_GCC_64bit-Debug/timerwater.ini')
+        ipfaucetnodemcu = config.get(devi1,'ipfaucetnodemcu')
+        ippumpnodemcu = config.get(devi1,'ippumpnodemcu')
+        portfaucet = config.get(devi1,'portfaucet')
+        portpump = config.get(devi1,'portpump')
+        max1 = config.get(devi1,'max')
+        min1 = config.get(devi1,'min')
 
-# Commit your changes in the database
-#       db.commit()
-#       html = open("calc.html".read())
-        result = "<html><head>Content-Type: text/html; charset=UTF-8\n\n[("+str(devi1)+","+str(val1)+")]"
+        if(not(ipfaucetnodemcu=="")&(ippumpnodemcu=="")):
+            if(ipfaucetnodemcu==ippumpnodemcu):
+                f = open(ipfaucetnodemcu+".io", 'w')
+                if(int(val1)>=max1):
+                    f.write(portfaucet+",ON")
+                    f.write(portpump+",ON")
+                else:
+                    f.write(portfaucet+",OFF")
+                    f.write(portpump+",OFF")
+                f.close()
+            else:
+                f = open(ipfaucetnodemcu+".io", 'w')
+                if(int(val1)>=max1):
+                    f.write(portfaucet+",ON")
+                else:
+                    f.write(portfaucet+",OFF")
+                f.close()
+                f = open(ippumpnodemcu+".io", 'w')
+                if(int(val1)>=max1):
+                    f.write(portpump+",ON")
+                else:
+                    f.write(portpump+",OFF")
+                f.close()
+            
+        if(ippumpnodemcu!=""):
+            f = open(ippumpnodemcu+".io", 'w')
+            if(int(val1)>=max1):
+                f.write(portpump+",ON")
+            else:
+                f.write(portpump+",OFF")
+            f.close()
+        if(ipfaucetnodemcu!=""):
+            f = open(ipfaucetnodemcu+".io", 'w')
+            if(int(val1)>=max1):
+                f.write(portfaucet+",ON")
+            else:
+                f.write(portfaucet+",OFF")
+            f.close()
+        if(os.path.isfile(nodemcuid+".io")):
+                f = open(nodemcuid+".io", 'r')
+                aaa = f.readline()
+                bbb = aaa.split(',')
+                f.close()
+
+        #if(int(val1)>80): val1="ON"
+        #else: val1="OFF"
+        result = "<html><head>Content-Type: text/html; charset=UTF-8\n\n[("+str(devi1)+","+str(val1)+";)]"
 #       header = "Content-Type: text/html; charset=UTF-8\n\n"
-#       output = header + html.format(result=result)
 #       result "Hello World  "+str(devi1)+","+str(val1)
         print result
 #       print "Content-type:text/html\r\n\r\n"
-#       print "<html>"
-#       print "<head><title>MyFirst CGI :) </title></head>"
-#       print "<body>"
-#       print "<p>It works!!!</p>"
         #for i in range(5):
 
 #       for field in form.keys():
 #               print "<tr><td>%s<td>%s" % (field, form[field].value)
-
         #print "<h1> hello world "+str(devi1)+" "+str(val1)
 #       print "</body>"
 #       print "</html>"
+        #print (result)
+except:
+        print "Error id "+id
+# disconnect from server
+#db.close()
+
---
+++ py/test_read_ini.py
@@ -0,0 +1,70 @@
+import os
+import cgi, cgitb
+import ConfigParser
+from datetime import datetime
+
+cgitb.enable()
+
+val1=80
+
+config = ConfigParser.ConfigParser()
+config.read('/home/ekapop/build-timerwater-Desktop_Qt_5_5_0_GCC_64bit-Debug/timerwater.ini')
+ipfaucetnodemcu = config.get('Sensor1','ipfaucetnodemcu')
+ippumpnodemcu = config.get('Sensor1','ippumpnodemcu')
+portfaucet = config.get('Sensor1','portfaucet')
+portpump = config.get('Sensor1','portpump')
+max1 = config.get('Sensor1','max')
+min1 = config.get('Sensor1','min')
+
+data1="-"
+data2="-"
+
+if(not(ipfaucetnodemcu=="")&(ippumpnodemcu=="")):
+    if(ipfaucetnodemcu==ippumpnodemcu):
+        f = open(ipfaucetnodemcu+".io", 'w')
+        if(int(val1)>=max1):
+            f.write(portfaucet+",ON")
+            f.write(portpump+",ON")
+        else:
+            f.write(portfaucet+",OFF")
+            f.write(portpump+",OFF")
+        f.close()
+    else:
+        f = open(ipfaucetnodemcu+".io", 'w')
+        if(int(val1)>=max1):
+            f.write(portfaucet+",ON")
+        else:
+            f.write(portfaucet+",OFF")
+        f.close()
+        f = open(ippumpnodemcu+".io", 'w')
+        if(int(val1)>=max1):
+            f.write(portpump+",ON")
+        else:
+            f.write(portpump+",OFF")
+        f.close()
+
+    
+if(ippumpnodemcu!=""):
+    f = open(ippumpnodemcu+".io", 'w')
+    if(int(val1)>=max1):
+        f.write(portpump+",ON")
+    else:
+        f.write(portpump+",OFF")
+    f.close()
+if(ipfaucetnodemcu!=""):
+    f = open(ipfaucetnodemcu+".io", 'w')
+    if(int(val1)>=max1):
+        f.write(portfaucet+",ON")
+    else:
+        f.write(portfaucet+",OFF")
+    f.close()
+if(os.path.isfile(ippumpnodemcu+".io")):
+	f = open(ippumpnodemcu+".io", 'r')
+	aaa = f.readline()
+	bbb = aaa.split(',')
+	#ccc = aaa.split(',',1)
+	f.close()   
+	print bbb[0]
+	print bbb[1]
+#
+#	vvv
